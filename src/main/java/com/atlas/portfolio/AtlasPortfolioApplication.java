c10cd4d1c907d4908b5a9d7fd3c96826ddc82b7b
Anatolii Gorbenko <anatoliydrake@gmail.com>
2025-10-24 19:39:27 +0700

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/atlas/portfolio/AtlasPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.atlas.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atlas/portfolio/AtlasPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlas.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
